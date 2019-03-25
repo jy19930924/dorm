57476b14962238350247527f5cc9c7955f25b6c8
jy19930924 <48906883+jy19930924@users.noreply.github.com>
2019-03-25 22:46:32 +0800

    Update sushe.sql

--- dorm-web/src/main/webapp/WEB-INF/sql/sushe.sql
+++ dorm-web/src/main/webapp/WEB-INF/sql/sushe.sql
@@ -2,70 +2,84 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50722
+Source Server Version : 50129
 Source Host           : localhost:3306
-Source Database       : sushe
+Source Database       : dorm
 
 Target Server Type    : MYSQL
-Target Server Version : 50722
+Target Server Version : 50129
 File Encoding         : 65001
 
-Date: 2019-03-25 18:15:53
+Date: 2019-03-25 22:49:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for admin
--- ----------------------------
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
-  `Admin_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Admin_Username` varchar(20) DEFAULT NULL,
-  `Admin_Password` varchar(20) DEFAULT NULL,
-  `Admin_Name` varchar(20) DEFAULT NULL,
-  `Admin_Sex` varchar(10) DEFAULT NULL,
-  `Admin_Tel` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Admin_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for building
 -- ----------------------------
 DROP TABLE IF EXISTS `building`;
 CREATE TABLE `building` (
-  `Building_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Building_Name` varchar(50) DEFAULT NULL,
-  `Building_Introduction` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`Building_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `BUILDING_NUM` varchar(32) DEFAULT NULL COMMENT '宿舍编号',
+  `NAME` varchar(32) DEFAULT NULL COMMENT '宿舍楼名称',
+  `CHECK_IN_NUMBER` int(11) DEFAULT NULL COMMENT '入住人数',
+  `FREE_NUMBER` int(11) DEFAULT NULL COMMENT '空余人数',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(32) DEFAULT NULL,
+  `UPDATE_TIME` datetime DEFAULT NULL,
+  `UPDATE_USER` varchar(32) DEFAULT NULL,
+  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '删除标志',
+  PRIMARY KEY (`DB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='宿舍楼';
 
 -- ----------------------------
--- Table structure for domitory
+-- Table structure for check_work
 -- ----------------------------
-DROP TABLE IF EXISTS `domitory`;
-CREATE TABLE `domitory` (
-  `Domitory_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Domitory_BuildingID` int(11) DEFAULT NULL,
-  `Domitory_Name` varchar(20) DEFAULT NULL,
-  `Domitory_Type` varchar(20) DEFAULT NULL,
-  `Domitory_Number` varchar(20) DEFAULT NULL,
-  `Domitory_Tel` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Domitory_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `check_work`;
+CREATE TABLE `check_work` (
+  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CHECK_TYPE` int(1) DEFAULT NULL COMMENT '检查类型 0考勤 1 卫生 2 纪律',
+  `SCORE` varchar(32) DEFAULT NULL COMMENT '评分',
+  `DORM_NUM` varchar(32) DEFAULT NULL COMMENT '宿舍号',
+  `REMARK` varchar(255) DEFAULT NULL COMMENT '备注',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`DB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='考勤';
+
+-- ----------------------------
+-- Table structure for dormitory
+-- ----------------------------
+DROP TABLE IF EXISTS `dormitory`;
+CREATE TABLE `dormitory` (
+  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DORM_NUM` varchar(32) DEFAULT NULL COMMENT '宿舍编号',
+  `NAME` varchar(32) DEFAULT NULL COMMENT '宿舍名称',
+  `CHECK_IN_NUMBER` int(11) DEFAULT NULL COMMENT '入住人数',
+  `FREE_NUMBER` int(11) DEFAULT NULL COMMENT '空余人数',
+  `BULIDING_NUM` varchar(32) DEFAULT NULL COMMENT '宿舍楼ID',
+  `CLASS_GRADE` varchar(32) DEFAULT NULL COMMENT '班级/年级',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(32) DEFAULT NULL,
+  `UPDATE_TIME` datetime DEFAULT NULL,
+  `UPDATE_USER` varchar(32) DEFAULT NULL,
+  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '删除标志',
+  PRIMARY KEY (`DB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='宿舍';
 
 -- ----------------------------
 -- Table structure for log
 -- ----------------------------
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE `log` (
-  `Log_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Log_StudentID` int(11) DEFAULT NULL,
-  `Log_TeacherID` int(11) DEFAULT NULL,
-  `Log_Date` varchar(50) DEFAULT NULL,
-  `Log_Remark` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`Log_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LOG_TYPE` int(1) DEFAULT NULL COMMENT '日志类型',
+  `CONTENT` text COMMENT '日志内容',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`DB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='日志';
 
 -- ----------------------------
 -- Table structure for m_menu
@@ -73,16 +87,19 @@ CREATE TABLE `log` (
 DROP TABLE IF EXISTS `m_menu`;
 CREATE TABLE `m_menu` (
   `DB_ID` varchar(32) NOT NULL,
+  `MENU_NUM` varchar(32) DEFAULT NULL COMMENT '菜单编号',
   `MENU_HREF` varchar(50) DEFAULT NULL COMMENT '菜单地址',
+  `MENU_ICON` varchar(255) DEFAULT NULL COMMENT '菜单图标',
   `MENU_NAME` varchar(32) DEFAULT NULL COMMENT '菜单名称',
-  `PARENT_MENU_ID` varchar(32) DEFAULT NULL,
+  `PARENT_MENU_ID` varchar(32) DEFAULT NULL COMMENT '父菜单id',
+  `IS_ENABLED` int(1) DEFAULT NULL COMMENT '启用标志',
   `CREATE_TIME` datetime DEFAULT NULL,
   `UPDATE_TIME` datetime DEFAULT NULL,
   `CREATE_USER` varchar(32) DEFAULT NULL,
   `UPDATE_USER` varchar(32) DEFAULT NULL,
   `DEL_FLAG` int(1) DEFAULT '1',
   PRIMARY KEY (`DB_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单';
 
 -- ----------------------------
 -- Table structure for m_role
@@ -90,103 +107,59 @@ CREATE TABLE `m_menu` (
 DROP TABLE IF EXISTS `m_role`;
 CREATE TABLE `m_role` (
   `DB_ID` varchar(32) NOT NULL,
-  `ROLE_NAME` varchar(32) DEFAULT NULL,
-  `REMARK` varchar(512) DEFAULT NULL,
-  `is_enabled` int(1) DEFAULT '0' COMMENT '启用状态 1是启用 0是禁用',
-  `CREATE_TIME` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '创建人',
-  `UPDATE_TIME` timestamp NULL DEFAULT NULL COMMENT '更新时间',
-  `UPDATE_USER` varchar(64) DEFAULT NULL COMMENT '修改人',
-  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '逻辑删除\r\n默认是1\r\n1正常\r\n0删除',
+  `ROLE_NAME` varchar(32) DEFAULT NULL COMMENT '角色名称',
+  `IS_ENABLED` int(1) DEFAULT '1' COMMENT '是否启用1 启用',
+  `LEVEL` varchar(32) DEFAULT NULL COMMENT '角色等级',
+  `MENU_ID` varchar(256) DEFAULT NULL COMMENT '菜单id，多个菜单用，分开',
+  `REMARK` varchar(512) DEFAULT NULL COMMENT '备注',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '������',
+  `UPDATE_TIME` datetime DEFAULT NULL,
+  `UPDATE_USER` varchar(64) DEFAULT NULL COMMENT '�޸���',
+  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '删除标志',
   PRIMARY KEY (`DB_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色';
 
 -- ----------------------------
 -- Table structure for m_user
 -- ----------------------------
 DROP TABLE IF EXISTS `m_user`;
 CREATE TABLE `m_user` (
-  `DB_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `NAME` varchar(64) DEFAULT NULL COMMENT '显示名',
-  `USER_NAME` varchar(64) NOT NULL COMMENT '登录名',
-  `PASSWORD` varchar(64) NOT NULL COMMENT '密码',
-  `TEL` varchar(20) DEFAULT NULL COMMENT '手机号',
-  `EMAIL` varchar(64) DEFAULT NULL COMMENT '用户邮箱',
-  `STATE` int(1) DEFAULT '1' COMMENT '启用状态，启用=1，禁用=0',
-  `LEVE_ID` int(2) DEFAULT NULL COMMENT '级别',
-  `LEVE_NAME` varchar(64) DEFAULT NULL COMMENT '等级名称',
-  `ROLE_ID` varchar(2) DEFAULT NULL COMMENT '角色',
-  `ROLE_NAME` varchar(64) DEFAULT NULL COMMENT '角色名称',
-  `LAST_LOGIN` timestamp NULL DEFAULT NULL COMMENT '最后登录时间',
-  `CREATE_TIME` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '创建人',
-  `UPDATE_TIME` timestamp NULL DEFAULT NULL COMMENT '修改时间',
-  `UPDATE_USER` varchar(64) DEFAULT NULL COMMENT '修改人',
-  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '删除标志',
+  `DB_ID` int(32) NOT NULL AUTO_INCREMENT,
+  `LOGIN_NAME` varchar(32) DEFAULT NULL COMMENT '登录名',
+  `NICK_NAME` varchar(32) NOT NULL COMMENT '用户昵称',
+  `PASSWORD` varchar(64) NOT NULL COMMENT '����',
+  `PHONE` varchar(11) DEFAULT NULL,
+  `EMAIL` varchar(32) DEFAULT NULL,
+  `ROLE_ID` varchar(2) DEFAULT NULL COMMENT '��ɫ',
+  `ROLE_NAME` varchar(64) DEFAULT NULL COMMENT '��ɫ����',
+  `LAST_LOGIN` timestamp NULL DEFAULT NULL COMMENT '����¼ʱ��',
+  `CREATE_TIME` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '����ʱ��',
+  `CREATE_USER` varchar(64) DEFAULT NULL COMMENT '������',
+  `UPDATE_TIME` timestamp NULL DEFAULT NULL COMMENT '�޸�ʱ��',
+  `UPDATE_USER` varchar(64) DEFAULT NULL COMMENT '�޸���',
+  `DEL_FLAG` int(1) DEFAULT '1' COMMENT 'ɾ����־',
   PRIMARY KEY (`DB_ID`),
-  UNIQUE KEY `USER_NAME` (`USER_NAME`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=111305 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
-
--- ----------------------------
--- Table structure for m_user_role
--- ----------------------------
-DROP TABLE IF EXISTS `m_user_role`;
-CREATE TABLE `m_user_role` (
-  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `MENU_ID` varchar(32) DEFAULT NULL,
-  `ROLE_ID` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`DB_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色权限关联表';
-
--- ----------------------------
--- Table structure for out1
--- ----------------------------
-DROP TABLE IF EXISTS `out1`;
-CREATE TABLE `out1` (
-  `Out_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Out_StudentID` varchar(11) DEFAULT NULL,
-  `Out_Date` varchar(50) DEFAULT NULL,
-  `Out_Remark` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`Out_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `USER_NAME` (`NICK_NAME`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='用户表';
 
 -- ----------------------------
 -- Table structure for student
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
-  `Student_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Student_DomitoryID` int(11) DEFAULT NULL,
-  `Student_Username` varchar(20) DEFAULT NULL,
-  `Student_Password` varchar(20) DEFAULT NULL,
-  `Student_Name` varchar(20) DEFAULT NULL,
-  `Student_Sex` varchar(20) DEFAULT NULL,
-  `Student_Class` varchar(20) DEFAULT NULL,
-  `Student_State` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Student_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for tb
--- ----------------------------
-DROP TABLE IF EXISTS `tb`;
-CREATE TABLE `tb` (
-  `TB_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `TB_TeacherID` int(11) DEFAULT NULL,
-  `TB_BuildingID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`TB_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for teacher
--- ----------------------------
-DROP TABLE IF EXISTS `teacher`;
-CREATE TABLE `teacher` (
-  `Teacher_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Teacher_Username` varchar(20) DEFAULT NULL,
-  `Teacher_Password` varchar(20) DEFAULT NULL,
-  `Teacher_Name` varchar(20) DEFAULT NULL,
-  `Teacher_Sex` varchar(10) DEFAULT NULL,
-  `Teacher_Tel` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Teacher_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `DB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `STUDENT_NUM` varchar(32) DEFAULT NULL COMMENT '学生编号',
+  `NAME` varchar(32) DEFAULT NULL,
+  `GENDER` int(1) DEFAULT NULL COMMENT '性别 0：女 1：男',
+  `PHONE` varchar(11) DEFAULT NULL COMMENT '电话',
+  `CLASS_GRADE` varchar(32) DEFAULT NULL COMMENT '班级/年级',
+  `ADDRESS` varchar(255) DEFAULT NULL COMMENT '家庭住址',
+  `DORM_NUM` varchar(32) DEFAULT NULL COMMENT '宿舍id',
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `CREATE_USER` varchar(32) DEFAULT NULL,
+  `UPDATE_TIME` datetime DEFAULT NULL,
+  `UPDATE_USER` varchar(32) DEFAULT NULL,
+  `DEL_FLAG` int(1) DEFAULT '1' COMMENT '删除标志',
+  PRIMARY KEY (`DB_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生表';
